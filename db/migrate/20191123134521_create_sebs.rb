b59ca892d64d23aaa0b74a6264e1493c379563b5
Louis Deboutin <louis.deboutin@gmail.com>
2019-11-24 00:58:04 +0100

    azert

---
+++ db/migrate/20191123134521_create_sebs.rb
@@ -0,0 +1,10 @@
+class CreateSebs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sebs do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_134854) do
+ActiveRecord::Schema.define(version: 2019_11_23_134521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2019_11_22_134854) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sebs", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
